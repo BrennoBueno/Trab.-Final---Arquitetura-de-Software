10a66b12e7f567bbdb0fc3374ed04654f9ddae5a
Bianca Mendes <bv42648@gmail.com>
2026-01-09 20:10:10 -0400

    tentativa de corrigir create rental

--- src/application/useCases/createRental/CreateRentalUseCase.ts
+++ src/application/useCases/createRental/CreateRentalUseCase.ts
@@ -11,19 +11,22 @@ export class CreateRentalUseCase {
     @inject(TYPES.RentalRepository) private rentalRepository: IRentalRepository
   ) {}
 
-  async execute(user_id: string, car_id: string, expected_return_date: Date): Promise<Rental> {
+  async execute(
+    user_id: string,
+    car_id: string,
+    expected_return_date: Date
+  ): Promise<Rental> {
     const minimumHour = 24;
 
     const car = await this.carRepository.findById(car_id);
     if (!car) {
       throw new Error("Carro não encontrado");
     }
-
-    if (car.available = false) { 
+    if (car.available === false) {
       throw new Error("Carro indisponível");
     }
 
-    const carHasRental = await this.rentalRepository.findOpenByCar(user_id); 
+    const carHasRental = await this.rentalRepository.findOpenByCar(car_id);
     if (carHasRental) {
       throw new Error("Carro já está alugado");
     }
@@ -32,13 +35,14 @@ export class CreateRentalUseCase {
     const compare = expected_return_date.getTime() - dateNow.getTime();
     const hours = compare / (1000 * 60 * 60);
 
-    if (hours < minimumHour - 1) {
+    if (hours < minimumHour) {
       throw new Error("Duração do aluguel deve ser de no mínimo 24 horas");
     }
+
     const rental = new Rental(car_id, user_id, expected_return_date);
     
     await this.rentalRepository.create(rental);
-    await this.carRepository.updateAvailable(car_id, false); 
-    return rental; 
+    await this.carRepository.updateAvailable(car_id, false);
+    return rental;
   }
 }
